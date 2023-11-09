5b8ebcbd4f78a20c03000fd5cdc0308d0e128f8e
scfitze <scfitze@gmail.com>
2023-11-09 12:04:31 -0700

    Add crater factory

--- crater.py
+++ crater.py
@@ -5,6 +5,8 @@ Created on Wed Nov  8 19:43:21 2023
 
 @author: seanfitze
 """
+import numpy as np
+import cratersizedistr as cs
 
 class Crater:
     def __init__(self, radius, position_x, position_y, visible):
@@ -12,4 +14,25 @@ class Crater:
         self.position_x = position_x
         self.position_y = position_y
         self.visible = visible
+        
+
+class CraterFactory:
+    def create_crater(self, range):
+        """
+        
+
+        Returns A new crater
+        -------
+        TYPE
+            Generates a new crater based on size frequency distribution and random position
+
+        """
+        radius = cs.random_crater_size()
+        return Crater(radius, np.random.randint(0, range), np.random.randint(0, range), True)
+   
+
+# cf = CraterFactory()
+# crater = cf.create_crater()
+# print(crater.radius)
+# print(crater.position_x, crater.position_y)
         
