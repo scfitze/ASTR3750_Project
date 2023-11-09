61dd12873390a133e6e46465ceee6b72243ab090
scfitze <scfitze@gmail.com>
2023-11-08 19:47:00 -0700

    Add crater file

---
+++ crater.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  8 19:43:21 2023
+
+@author: seanfitze
+"""
+
+class Crater:
+    def __init__(self, radius, position_x, position_y, visible):
+        self.radius = radius
+        self.position_x = position_x
+        self.position_y = position_y
+        self.visible = visible
+        
