69a4e654e12a457ffab301225788fa8a4c487539
Julia Johnston <jrojohnston@yahoo.com>
2023-11-29 20:45:47 -0700

    Add size frequency distribution

--- cratersizedistr.py
+++ cratersizedistr.py
@@ -1,2 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+def N():
+    return 50*(np.linspace(10, 150))**(-1.8)
+
 def random_crater_size():
-    return 10 #km
+    size = np.round(((N())/50)**(-1/1.8))
+    return random.choice(size)
